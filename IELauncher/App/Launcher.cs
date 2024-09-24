6d9b6b90f67789b303a328cd8f418efdcf84eebd
Tristian Radford <tristian.radford@gmail.com>
2024-09-24 19:24:28 +0100

    Initial

---
+++ IELauncher/App/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace IELauncher.App
+{
+    class Launcher
+    {
+        public void LaunchIE(string url)
+        {
+            ShowIE(url);
+        }
+
+        private int ShowIE(string url)
+        {
+            SHDocVw.InternetExplorer IE = new SHDocVw.InternetExplorer()
+            {
+                Visible = true,
+
+            };
+
+            IE.Navigate2(url, 0);
+
+            return 0;
+        }
+    }
+}
---
+++ IELauncher/Program.cs
@@ -0,0 +1,23 @@
+﻿using IELauncher.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IELauncher
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            var url = "about:blank";
+            if (args.Length > 0) {
+                url = args[0];
+            }
+
+            (new Launcher()).LaunchIE(url);
+            return 0;
+        }
+    }
+}
